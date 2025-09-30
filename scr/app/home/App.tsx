9c2df7e2b5adea86f280f003a8dd80e8d97e1fc5
fernandaoooo <mariafernandamenezes002@gmail.com>
2025-09-30 08:07:24 -0400

    30

--- scr/app/home/App.tsx
+++ scr/app/home/App.tsx
@@ -27,20 +27,3 @@ export default function App(){
     );
 }
 
-// export default function App() {
-//     return (
-//         <View style={styles.container}>
-//             <Text style={styles.text}>Nome Evento</Text>
-//             <View style={styles.form}>
-//             <TextInput style={styles.input} placeholder='Digite o evento' placeholderTextColor={'#fff'}></TextInput>
-//             <TouchableOpacity  style={styles.button} onPress={()=>{Alert.alert("Teste Botão")}}>
-//                 <Text style={styles.buttonText}>+</Text>
-//             </TouchableOpacity>
-//             </View>
-//             <Event name="Evento novo"/>
-//             <Event name="Evento cancelado"/>
-
-//         </View>
-        
-//     )
-// }
